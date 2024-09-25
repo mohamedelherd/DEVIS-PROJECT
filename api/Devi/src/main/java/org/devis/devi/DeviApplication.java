2bd34ad3d3bce08d603b73bab349541440bc81ed
ABDLATIF12BOUBAL <abdellatif.boubal@edu.uiz.ac.ma>
2024-09-25 15:09:09 +0100

    main

---
+++ api/Devi/src/main/java/org/devis/devi/DeviApplication.java
@@ -0,0 +1,13 @@
+package org.devis.devi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeviApplication.class, args);
+    }
+
+}
---
+++ api/Devi/src/test/java/org/devis/devi/DeviApplicationTests.java
@@ -0,0 +1,13 @@
+package org.devis.devi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
